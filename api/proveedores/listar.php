e581176f47a28df601f6a7333e3b39987d5d6736
kevinfabianp <kevin.fabian@rjberru.com>
2021-11-25 18:03:58 -0500

    headers que el tarao de miguel no vió

--- api/proveedores/listar.php
+++ api/proveedores/listar.php
@@ -3,7 +3,7 @@
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: GET");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, User,X-Requested-With");
 
     include_once '../../clases/Usuario.php';
     include_once '../../clases/Proveedor.php';
